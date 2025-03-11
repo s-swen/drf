6a628f4071261436ce403970f0c141d305d5c212
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-03-11 00:25:29 +0000

    csrf

--- tut/tut/settings.py
+++ tut/tut/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost:8000",
+]
